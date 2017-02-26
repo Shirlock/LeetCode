09c821fb8cda0ae46171717766963b72158c573d
Shirlock <1203958766@qq.com>
2017-02-26 11:14:17 +0800

    Update 4_Median_of _two_Sorted_Arrays.cpp

--- 4_Median_of _two_Sorted_Arrays.cpp
+++ 4_Median_of _two_Sorted_Arrays.cpp
@@ -2,32 +2,45 @@
 //  4_Median_of _two_Sorted_Arrays.cpp
 //  
 //
-//  Created by songyu on 2017/2/25.
+//  Created by songyu on 2017/2/26.
 //
 //
+
+/* 没想出好方法，参考LeetCode 上的解法*/
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len1=nums1.size()-1;
-        int len2=nums2.size()-1;
-        int low1=0,high1=len1,low2=0,high2=len2,middle1,middle2;
-        while(low1<=high1||low2<=high2){
-            if(low1<=high1)
-                middle1=(low1+high1)/2;
-            if(low2<=high2)
-                middle2=(low2+high2)/2;
-            //   if(middle2==middle1)
-            //  return (nums1[middle1]+nums2[middle2])/2.0;
-            //        else
-            if(middle2>middle1){
-                high2=middle2-1;
-                low1=middle1+1;
-            }else{
-                high1=middle1-1;
-                low2=middle2+1;
-            }
+        vector<int> numsA,numsB;
+        if(nums1.size()>nums2.size()){
+            numsA=nums2;
+            numsB=nums1;
+        }else {
+            numsA=nums1;
+            numsB=nums2;
         }
-        return (nums1[middle1]+nums2[middle2])/2.0;
+        int lenA=numsA.size(),lenB=numsB.size();
+        int low=0,high=lenA,a,b;
+        while(low<=high){
+            a=(low+high)/2;
+            b=(lenA+lenB+1)/2-a;
+           if(a>0 &&numsA[a-1]>numsB[b]){
+               high=a-1;
+           }else if(a<lenA && numsA[a]<numsB[b-1]){
+               low=a+1;
+           }else {
+               int maxLeft,minRight;
+               if(a==0) maxLeft=numsB[b-1];
+               else if(b==0) maxLeft=numsA[a-1];
+               else maxLeft= max(numsA[a-1],numsB[b-1]);
+               
+               if(a==lenA) minRight=numsB[b];
+               else if(b==lenB) minRight=numsA[a];
+               else minRight= min(numsA[a],numsB[b]);
+              
+               if((lenA+lenB)%2==0) return (maxLeft+minRight)/2.0;
+               else return maxLeft;
+           }
+        }
     }
 };
